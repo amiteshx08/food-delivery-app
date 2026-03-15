7eab19fada5bc19225cde38f97005f00cbab9531
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-03-16 00:00:49 +0530

    feat: nested react elements

--- App.js
+++ App.js
@@ -1,3 +1,21 @@
-var heading = React.createElement("h1", { id: "heading" }, "Welcome to Food-Delivery!!");
-var root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(heading);
+"use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+// const heading = React.createElement(
+//     "h1", 
+//     {id: "heading"}, 
+//     "Welcome to Food-Delivery!!"
+// );
+var parent = React.createElement('div', { id: 'parent' }, [
+    React.createElement('div', { id: 'child1' }, [
+        React.createElement('h1', {}, 'I am an h1 tag'),
+        React.createElement('h2', {}, 'I am an h2 tag')
+    ]),
+    React.createElement('div', { id: 'child2' }, [
+        React.createElement('h1', {}, 'I am an h1 tag'),
+        React.createElement('h2', {}, 'I am an h2 tag')
+    ])
+]);
+console.log(parent);
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+// exports.default = {};
--- App.ts
+++ App.ts
@@ -1,12 +1,25 @@
 declare var React: any;
 declare var ReactDOM: any;
 
-const heading = React.createElement(
-    "h1", 
-    {id: "heading"}, 
-    "Welcome to Food-Delivery!!"
-);
+// const heading = React.createElement(
+//     "h1", 
+//     {id: "heading"}, 
+//     "Welcome to Food-Delivery!!"
+// );
 
-const root = ReactDOM.createRoot(document.querySelector(".root"));
+const parent = React.createElement('div', {id:'parent'}, [
+    React.createElement('div', {id: 'child1'}, [
+        React.createElement('h1', {}, 'I am an h1 tag'),
+        React.createElement('h2', {}, 'I am an h2 tag')
+    ]),
+    React.createElement('div', {id: 'child2'}, [
+        React.createElement('h1', {}, 'I am an h1 tag'),
+        React.createElement('h2', {}, 'I am an h2 tag')
+    ])
+])
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
+
+export {}
