f42990db44e4534fe56039317165ef13974b025f
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-03-18 21:50:25 +0530

    feat: basic layout of web app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => {
-  return <h2 className="title">Food is arriving!</h2>;
-};
-
 const Heading = () => {
   return (
-    <div>
-      <h1 className="header">Food Delivery</h1>
-      <Title />
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="restaurant image"
+        src="https://b.zmtcdn.com/data/pictures/7/21317067/baac459e901bf025e7e6d5c03fe831b3_featured_v2.jpg?output-format=webp"
+      />
+      <h3>Koa Cafe</h3>
+      <h4>Italian,Thai Food,French,Indo-Japanese</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <span>Search</span>
+      </div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div>
+      <Heading />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-root.render(<Heading />);
+root.render(<AppLayout />);
