42e7bb77f868562ae3256f86da201cf9066ef52e
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-03-21 23:06:25 +0530

    feat: used react hook useEffect

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,19 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   const [listofrestaurants, setListOfRestaurants] = useState(resList);
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.2379843&lng=73.2029272&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json()
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle.restaurants)
+  }
   return (
     <div className="body">
       <div className="filter">
