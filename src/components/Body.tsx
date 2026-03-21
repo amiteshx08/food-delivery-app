eecb1813b7e34e0398600a98096305d3804973cf
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-03-21 23:53:53 +0530

    feat: added the login-logout btn

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,19 +1,36 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
+interface RestaurantInfo {
+    info: {
+      id: string;
+      name: string;
+      cuisines: string[];
+      avgRating: number;
+      cloudinaryImageId: string;
+    };
+}
 
 const Body = () => {
-  const [listofrestaurants, setListOfRestaurants] = useState(resList);
+  const [listofrestaurants, setListOfRestaurants] = useState<RestaurantInfo[]>([]);
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.2379843&lng=73.2029272&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-    const json = await data.json()
-    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle.restaurants)
-  }
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.2379843&lng=73.2029272&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle.restaurants,
+    );
+  };
+
+  if(listofrestaurants.length === 0) return <Shimmer />
   return (
     <div className="body">
       <div className="filter">
@@ -21,7 +38,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listofrestaurants.filter(
-              (res) => res.info.avgRating > 4.5
+              (res) => res.info.avgRating > 4.5,
             );
             setListOfRestaurants(filteredList);
           }}
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,12 +1,12 @@
 import { LOGO_URL } from "../utils/constant";
+import { useState } from "react";
+
 const Heading = () => {
+  const [btnName, setbtnName] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src={LOGO_URL}
-        />
+        <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-items">
         <ul>
@@ -15,6 +15,14 @@ const Heading = () => {
           <li>About Us</li>
           <li>Cart</li>
         </ul>
+        <button
+          className="login-logout-btn"
+          onClick={() => {
+            btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
+          }}
+        >
+         {btnName}
+        </button>
       </div>
     </div>
   );
